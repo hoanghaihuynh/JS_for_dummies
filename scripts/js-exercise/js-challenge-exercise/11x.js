7c9818de0612705c14dfdcd7fff8cd0b544498ea
harryOsporn <tinvatit4869@gmail.com>
2024-08-01 18:32:20 +0700

    11x-challenge-exercise

---
+++ scripts/js-exercise/js-challenge-exercise/11x.js
@@ -0,0 +1,60 @@
+
+const todoList = [
+    {
+        name: 'nấu cơm',
+        dueDate: '2024-10-21'
+    }, 
+    {
+        name: 'rửa chén',
+        dueDate: '2023-03-30'
+    }    
+];
+
+renderTodoList();
+
+function renderTodoList() {
+    let todoHTML = '';
+
+    for (let i = 0; i < todoList.length; i++) {
+        const todoObject = todoList[i];
+        const name = todoObject.name;
+        const dueDate = todoObject.dueDate;
+        // const { name, dueDate} = todoObject;
+        const html = 
+            `
+            <div>${name}</div>
+            <div>${dueDate}</div>
+            <div>
+                <button class="delete-btn" onclick="
+                    todoList.splice(${i}, 1);
+                    renderTodoList();
+                ">Delete</button>
+            </div>
+            `;
+        todoHTML += html;
+    }
+    document.querySelector('.js-todo-list').innerHTML = todoHTML;
+
+    console.log(todoHTML);
+}
+
+function addTodo() {
+    const inputElement = document.querySelector('.js-name-input');
+    const name = inputElement.value;
+
+    const dateInputElement = document.querySelector('.js-due-date-input');
+    const dueDate = dateInputElement.value;
+    
+    todoList.push(
+        {
+            // name: name,
+            // dueDate: date
+            name,
+            dueDate
+        }
+    );
+    console.log(todoList);
+    inputElement.value = '';
+    renderTodoList();
+}
+
